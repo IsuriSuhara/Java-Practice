c68c8a9cf61f2d2d396e44c3b977a223f7225a76
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-07-23 19:04:14 +0530

    Create CleanCode.java
    Clean code for repeated lines.

---
+++ CleanCode.java
@@ -0,0 +1,18 @@
+public class CleanCode {
+    public static void sum(int x, int y){
+       System.out.println("Let's try addition! ");
+       int z = x + y;
+       System.out.println("The sum is "+z); 
+    }
+    
+    public static void main(String[] args) {
+        
+        sum(1,2);
+        sum(3,4);
+        sum(5,6);
+        
+        
+    }
+    
+    
+}
