eb2b4450ff25458b1baea1d1f4edbe3eb1705617
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-08-23 04:27:34 -0700

    Create OCA_Chapter1.Animal.java

---
+++ OCA_Chapter1.Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    private String name;
+    //Getter
+    public String geName() {
+        return name;
+    }
+    //Setter
+    public void settName(String newName) {
+        this.name = newName;
+    }
+
+}
+
