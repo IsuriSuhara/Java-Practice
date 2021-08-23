79480c797dab2e3df1f770f6947c868d5ebd9879
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-08-23 04:28:40 -0700

    Create OCA_Chapter1.Main.java

---
+++ OCA_Chapter1.Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        myAnimal.settName("Shane");
+        System.out.println(myAnimal.geName());
+
+    }
+}
