7a17f22d8acba42ba0b6650f0d5cee33710acfb9
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-08-26 09:50:13 -0700

    Create booleanDefault.compileOrNot.java
    Code compile only if both variables are initialized in both if and else code blocks.

---
+++ booleanDefault.compileOrNot.java
@@ -0,0 +1,19 @@
+public class compileOrNot {
+    public static void findAnswer(boolean check) {
+        int answer;
+        int onlyOneBranch;
+        if (check) {
+            onlyOneBranch = 1;
+            answer = 1;
+        } else {
+            answer = 2;
+            //onlyOneBranch = 0;
+        }
+        System.out.println(answer);
+        System.out.println(onlyOneBranch); // DOES NOT COMPILE
+    }
+
+    public static void main(String[] args) {
+        findAnswer(true);//java: variable onlyOneBranch might not have been initialized
+    }
+}
