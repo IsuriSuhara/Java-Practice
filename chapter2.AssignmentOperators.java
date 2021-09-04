857e401cffacd877938891edf56030832b8dfe1e
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-09-04 05:09:27 -0700

    Update chapter2.AssignmentOperators.java

--- chapter2.AssignmentOperators.java
+++ chapter2.AssignmentOperators.java
@@ -18,8 +18,29 @@ public class AssignmentOperators {
         //Casting Primitive values
         int x = (int)1.0;
         short y = (short)1921222; // Stored as 20678
+        //overflow happens.Overflow is when a number is so large that it will no longer fit
+        //within the data type, so the system “wraps around” to the next lowest value and counts
+        //up from there.
         int z = (int)9L;
         long t = 192301398193810323L;
         System.out.println(x+" "+y+" "+z+" "+t);
+
+        System.out.println(2147483647+1); // -2147483648
+        //Since 2147483647 is the maximum int value, adding any strictly positive value to it will
+        //cause it to wrap to the next negative number.
+
+        short i = 10;
+        short j = 3;
+        short k = (short)(i * j);
+        //By performing this explicit cast of a larger value into a smaller data type, you are
+        //instructing the compiler to ignore its default behavior. In other words, you are telling the
+        //compiler that you have taken additional steps to prevent overflow or underflow. It is also
+        //possible that in your particular application and scenario, overflow or underflow would
+        //result in acceptable values.
+        System.out.println(k);
+        //short k = i * j; // DOES NOT COMPILE
+        //short values are automatically
+        //promoted to int when applying any arithmetic operator with the resulting value being of
+        //type int. Trying to set a short variable to an int results in a compiler error.
     }
 }
