ad8844369b6a1705991361c33d0a27a4e66ffd2a
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-09-04 05:27:53 -0700

    Create chapter2.RelationalOperators.java

---
+++ chapter2.RelationalOperators.java
@@ -0,0 +1,15 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 10, y = 20, z = 10;
+        System.out.println(x < y); // Outputs true
+        System.out.println(x <= y); // Outputs true
+        System.out.println(x >= z); // Outputs true
+        System.out.println(x > z); // Outputs false
+
+        //If the two numeric operands are not of the same data type, the
+        //smaller one is promoted.
+
+        //instanceof operator is applied to object references and classes or
+        //interfaces.
+    }
+}
