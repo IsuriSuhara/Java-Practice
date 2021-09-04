9f7367ba871f8135c91dfd18be98a144a85b5ff3
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-09-04 04:52:18 -0700

    Create chapter2.AssignmentOperators.java

---
+++ chapter2.AssignmentOperators.java
@@ -0,0 +1,18 @@
+public class AssignmentOperators {
+   
+    public static void main(String[] args) {
+       /*
+    int x = 1.0; // DOES NOT COMPILE
+    short y = 1921222; // DOES NOT COMPILE
+    int z = 9f; // DOES NOT COMPILE
+    long t = 192301398193810323; // DOES NOT COMPILE
+     */
+      
+        int x = 1;
+        double y = 1.0;
+        int z = 1921222;
+        int h = 9;
+        float i = 9f;
+        long t = 192301398193810323L;
+    }
+}
