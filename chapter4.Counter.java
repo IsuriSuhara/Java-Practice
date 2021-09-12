f195fd193417dcd2a15f02ce20feb337262ceae4
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-09-12 04:30:18 -0700

    Create chapter4.Counter.java

---
+++ chapter4.Counter.java
@@ -0,0 +1,10 @@
+public class Counter {
+    private static int count;
+    public Counter() { count++; }
+    public static void main(String[] args) {
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
+        System.out.println(count); // 3
+    }
+}
