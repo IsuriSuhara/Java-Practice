8c62a020f0e210d94ba6beeae5efba904cbb9fc3
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-09-16 03:02:55 -0700

    Create chapter4.CallInitializationOrderSimple.java

---
+++ chapter4.CallInitializationOrderSimple.java
@@ -0,0 +1,9 @@
+public class CallInitializationOrderSimple {
+    public static void main(String[] args) {
+        InitializationOrderSimple init = new InitializationOrderSimple();
+        //0
+        //10
+        //Torchie
+        //constructor
+    }
+}
