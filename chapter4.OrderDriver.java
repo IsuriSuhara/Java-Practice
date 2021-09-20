c68ae8b496fffad9b25dfc7055d9fdc714d77b68
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-09-20 00:37:17 -0700

    Create chapter4.OrderDriver.java

---
+++ chapter4.OrderDriver.java
@@ -0,0 +1,10 @@
+public class OrderDriver {
+    public static void main(String[] args) {
+        System.out.print(Q22Order.result+" ");
+        System.out.print(Q22Order.result+" ");
+        new Q22Order();
+        new Q22Order();
+        System.out.print(Q22Order.result+" ");
+        //u  u  ucrcr 
+    }
+}
