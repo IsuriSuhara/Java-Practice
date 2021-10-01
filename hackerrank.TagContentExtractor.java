470071b4e51e9b496e2e0910d8d6857d6f017b9e
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-09-30 23:58:11 -0700

    Create hackerrank.TagContentExtractor.java

---
+++ hackerrank.TagContentExtractor.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class TagContentExtractor{
+	public static void main(String[] args){
+       
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String line = in.nextLine();
+            //line=line.trim();
+            Matcher m = Pattern.compile("<(.+)>(([^<>]+))</\\1>").matcher(line);
+            boolean matchFound = false;
+            while (m.find()) {
+                System.out.println(m.group(2));
+                matchFound = true;
+            }
+            if ( ! matchFound) {
+                System.out.println("None");
+            }
+			testCases--;
+		}
+	}
+}
+
+
+
