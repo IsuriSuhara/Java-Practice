6da4ca43cc2949117541a6f1e239ef21a4969ca7
IsuriSuhara <56144961+IsuriSuhara@users.noreply.github.com>
2021-10-03 09:53:21 -0700

    Create hackerrank.JavaGenerics.java

---
+++ hackerrank.JavaGenerics.java
@@ -0,0 +1,40 @@
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+class Printer
+{
+   //Write your code here
+
+  public <T> void printArray(T[] pa) {
+      for(T i:pa){
+          System.out.println(i);
+      }
+      
+  }
+   
+ 
+}
+
+public class JavaGenerics {
+
+
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+      
+    }
+}
